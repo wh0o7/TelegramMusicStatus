d9a743bfa93723e7e5bc297626a5d289304eefb9
Andrey Kosinets <a.kosinets7@gmail.com>
2025-09-30 21:47:41 +0300

    expiremental fix: ym state

--- TelegramMusicStatus/Services/YandexMusicService.cs
+++ TelegramMusicStatus/Services/YandexMusicService.cs
@@ -14,14 +14,22 @@ public interface IYandexMusicService : IMusicService
 
 public sealed class YandexMusicService : IYandexMusicService
 {
-    private readonly YandexMusicApi _api;
-    private YnisonPlayer _player;
-    private readonly AuthStorage _storage;
-    private YandexMusicState? _state = null;
+    private YandexMusicApi _api;
+    private YnisonPlayer? _player = null;
+    private AuthStorage _storage;
+    private YandexMusicState? _state;
 
-    public YandexMusicService(IConfig<MainConfig> config)
+    public YandexMusicService()
     {
-        var ym = config.Entries.YandexMusicAccount;
+        this.GetPlayer();
+
+        Utils.WriteLine("Yandex Music client started!");
+    }
+
+    private void GetPlayer()
+    {
+        if (this._player is not null) this._player.Dispose();
+        var ym = new Config<YandexMusic?>().Entries;
 
         if (ym is null) return;
         this._storage = new AuthStorage { DeviceId = Guid.NewGuid().ToString() };
@@ -29,15 +37,7 @@ public sealed class YandexMusicService : IYandexMusicService
         this._api = new YandexMusicApi();
         this._api.User.Authorize(this._storage, ym.Token);
 
-
-        this.GetPlayer();
-        
-        Utils.WriteLine("Yandex Music client started!");
-    }
-    
-    private void GetPlayer()
-    {
-        Utils.WriteLine($"Getting player...\n Current Yandex user is {this._api.User.GetLoginInfo(this._storage).Login}");
+        Utils.WriteLine($"Getting player...\n Current Yandex user is {this._api.User.GetUserAuth(this._storage).Result.Account.Login.ToJson()}");
         this._player = this._api.Ynison.GetPlayer(this._storage);
         this._player.Connect();
         Thread.Sleep(5000);
@@ -53,6 +53,19 @@ public sealed class YandexMusicService : IYandexMusicService
     {
         try
         {
+            var userAuth = (await this._api.User.GetUserAuthAsync(this._storage)).Result.Account;
+            if (userAuth is null || this._player == null)
+            {
+                Console.WriteLine($"(Yandex Music)   Smth null: \n 1. userAuth is {(userAuth is null ? string.Empty : "not")} null.\n2. player is {(this._player is null ? string.Empty : "not")} null  \n\n\n");
+                this.GetPlayer();
+            }
+            
+            if (this._player is null)
+            {
+                Console.WriteLine("(Yandex Music)  PLAYER IS NULL 2-ND TIME IN A ROW!!!!!!!!!");
+                return (false, null);
+            }
+
             var status = this._player.State.PlayerState?.Status;
             var track = this._player.Current;
             var bio = $"{track.Title} - {string.Join(", ", track.Artists.Select(a => a.Name))}";
